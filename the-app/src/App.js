64083d7162934af86a57106e946ccc8e1ff5b12c
Tyler Hattori <t.hattori.seven@gmail.com>
2021-01-21 20:01:05 -0800

    idk I'm doing a lot of random stuff

--- the-app/src/App.js
+++ the-app/src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <Ball></Ball>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey this is our <span className= "website-text">~website~</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
