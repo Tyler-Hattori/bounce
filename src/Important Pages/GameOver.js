e8026a0c3d19eb13f87b62c9858eb2e800732d3a
Tyler Hattori <t.hattori.seven@gmail.com>
2021-03-02 16:29:31 -0800

    hm

--- src/Important Pages/GameOver.js
+++ src/Important Pages/GameOver.js
@@ -4,23 +4,30 @@ import database from '../firebase'
 
 function GameOver(props) {
     let [bestTime, setBestTime] = useState(0)
-    let [length, setLength] = useState("")
+    let lengthString = "string"
+    let difficultyString = "string"
 
-    if (props.length == 3) setLength("super short")
-    else if (props.length == 4) setLength("short")
-    else if (props.length == 5) setLength("standard")
-    else if (props.length == 7) setLength("long")
-    else if (props.length == 9) setLength("quite long")
-    else if (props.length == 12) setLength("very long")
+    if (props.length === "3 obstacles") lengthString = "super short"
+    else if (props.length === "4 obstacles") lengthString = "short"
+    else if (props.length === "5 obstacles") lengthString = "standard"
+    else if (props.length === "7 obstacles") lengthString = "long"
+    else if (props.length === "9 obstacles") lengthString = "quite long"
+    else if (props.length === "12 obstacles") lengthString = "very long"
+
+    if (props.difficulty == 1) difficultyString = "plebian"
+    else if (props.length == 2) difficultyString = "easy"
+    else if (props.length == 3) difficultyString = "medium"
+    else if (props.length == 4) difficultyString = "hard"
+    else if (props.length == 5) difficultyString = "expert"
 
     database.ref(props.length+'/'+props.difficulty).push(props.time)
     database.ref(props.length+'/'+props.difficulty).orderByValue().limitToFirst(1).once("value",(snapshot) => {
         snapshot.forEach((child)=>{setBestTime(child.val())})  })
-    database.ref(props.length+'/'+props.difficulty).orderByValue().once("value",(snapshot) => {
-        snapshot.forEach((child)=>{
-            if (child.val() > bestTime) database.ref(props.length+'/'+props.difficulty+'/'+child.key).remove()
-        })  
-    })
+    // database.ref(props.length+'/'+props.difficulty).orderByValue().once("value",(snapshot) => {
+    //     snapshot.forEach((child)=>{
+    //         if (child.val() > bestTime) database.ref(props.length+'/'+props.difficulty+'/'+child.key).remove()
+    //     })  
+    // })
     let newRecord = false
 
     if(bestTime == props.time) newRecord = true;
@@ -30,7 +37,7 @@ function GameOver(props) {
             <div className= "section">
                 <p style= {{fontSize: '6vmin'}}>Nice Work!</p>
                 <p style= {{fontSize: '4vmin'}}>Time: {props.time}s</p>
-                <p style= {{fontSize: '2vmin'}}>On a {length} {props.difficulty} course</p>
+                <p style= {{fontSize: '2vmin'}}>On a {lengthString} {difficultyString} course</p>
                 {newRecord ? <p>New Record!!</p>:<p>Best Time: {bestTime}</p>}
             </div>
             <button className = "new_game" onClick= {() => {document.location.reload()}}>New Game</button>
